e0dd14b77fe4dac47c24f32860830d5b6d13ef9b
petertr <peter.trifanov@mail.ru>
2016-11-17 21:25:05 +0300

    Second task - mless mistakes

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo ln -s home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
