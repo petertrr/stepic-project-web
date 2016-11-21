d235d9d1b187b98dcaca8d833bf77f4cf2b632ae
petertr <peter.trifanov@mail.ru>
2016-11-21 21:59:31 +0300

    Third task - #4

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-sudo rm /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-hello.conf /etc/gunicorn.d/test-hello
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
